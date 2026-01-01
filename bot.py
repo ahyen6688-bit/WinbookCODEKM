5ca621d666f4a4422524561d107a6aa9e6ac8910
ahyen6688-bit <ahyen6688@gmail.com>
2026-01-01 17:59:16 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -189,7 +189,7 @@ def main():
 
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("km", km, filters.ChatType.GROUPS))
+    app.add_handler(CommandHandler("km", km))
     app.add_handler(CallbackQueryHandler(handle_callback))
     app.run_polling()
 
